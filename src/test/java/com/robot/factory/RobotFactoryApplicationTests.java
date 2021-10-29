1f8c45591c06681ba75f6630fee37785293dc8d6
merve <mervekaymak1993@gmail.com>
2021-10-29 11:45:40 +0300

    Initializes robot factory challenge project

---
+++ src/test/java/com/robot/factory/RobotFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robot.factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
